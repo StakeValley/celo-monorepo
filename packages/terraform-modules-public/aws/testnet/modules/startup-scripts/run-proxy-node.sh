fe49703b65ab266da67f4e9288396fb970b4e3a3
Chris Geihsler <chris@geihsler.net>
2022-04-11 15:07:20 -0400

    fix: remove multiline docker run commands

--- packages/terraform-modules-public/aws/testnet/modules/startup-scripts/run-proxy-node.sh
+++ packages/terraform-modules-public/aws/testnet/modules/startup-scripts/run-proxy-node.sh
@@ -5,9 +5,11 @@ CELO_VALIDATOR_SIGNER_ADDRESS=${validator_signer_address}
 NODE_DIRECTORY=/home/ubuntu/celo-proxy-node
 
 mkdir $NODE_DIRECTORY
+mkdir $NODE_DIRECTORY/keystore
 cd $NODE_DIRECTORY
 
 PROXY_ADDRESS=${proxy_address}
+
 echo -n '${proxy_private_key_password}' > .password
 echo -n '${proxy_private_key_file_contents}' > keystore/${proxy_private_key_filename}
 echo -n '${proxy_node_private_key}' > .nodeprivatekey
@@ -22,10 +24,4 @@ else
 fi
 
 # Adapted from https://docs.celo.org/getting-started/baklava-testnet/running-a-validator-in-baklava#deploy-a-proxy
-docker run -d --name celo-proxy $DOCKER_LOGGING_PARAMS --restart unless-stopped \
-  -p 30303:30303 -p 30303:30303/udp -p 30503:30503 -p 30503:30503/udp \
-  -v $PWD:/root/.celo $CELO_IMAGE --verbosity 3 --syncmode full --nousb \ 
-  --proxy.proxy --proxy.proxiedvalidatoraddress $CELO_VALIDATOR_SIGNER_ADDRESS --proxy.internalendpoint :30503 \ 
-  --etherbase $PROXY_ADDRESS --unlock $PROXY_ADDRESS --password /root/.celo/.password \
-  --allow-insecure-unlock --baklava --datadir /root/.celo \
-  --celostats=${validator_name}@${ethstats_host}
+docker run -d --name celo-proxy $DOCKER_LOGGING_PARAMS --restart unless-stopped -p 30303:30303 -p 30303:30303/udp -p 30503:30503 -p 30503:30503/udp -v $PWD:/root/.celo $CELO_IMAGE --verbosity 3 --syncmode full --nousb  --proxy.proxy --proxy.proxiedvalidatoraddress $CELO_VALIDATOR_SIGNER_ADDRESS --proxy.internalendpoint :30503 --etherbase $PROXY_ADDRESS --unlock $PROXY_ADDRESS --password /root/.celo/.password --allow-insecure-unlock --baklava --datadir /root/.celo --celostats=${validator_name}@${ethstats_host}
--- packages/terraform-modules-public/aws/testnet/modules/startup-scripts/run-validator-node.sh
+++ packages/terraform-modules-public/aws/testnet/modules/startup-scripts/run-validator-node.sh
@@ -1,12 +1,14 @@
 # !/bin/bash
 CELO_IMAGE=${celo_image}
+CELO_VALIDATOR_SIGNER_ADDRESS=${validator_signer_address}
+
 NETWORK_ID=${celo_network_id}
 
 NODE_DIRECTORY=/home/ubuntu/celo-validator-node
 
 mkdir $NODE_DIRECTORY
+mkdir $NODE_DIRECTORY/keystore
 cd $NODE_DIRECTORY
-CELO_VALIDATOR_SIGNER_ADDRESS=${validator_signer_address}
 
 PROXY_ENODE=${proxy_enode}
 PROXY_INTERNAL_IP=${proxy_internal_ip}
@@ -25,9 +27,4 @@ else
 fi
 
 # Adapted from https://docs.celo.org/getting-started/baklava-testnet/running-a-validator-in-baklava#deploy-a-validator
-docker run -d --name celo-validator $DOCKER_LOGGING_PARAMS --restart unless-stopped --stop-timeout 300 \
-  -p 30303:30303 -p 30303:30303/udp -v $PWD:/root/.celo $CELO_IMAGE --verbosity 3 \
-  --syncmode full --mine --etherbase $CELO_VALIDATOR_SIGNER_ADDRESS --nodiscover --nousb \ 
-  --proxy.proxied --proxy.proxyenodeurlpair=enode://$PROXY_ENODE@$PROXY_INTERNAL_IP:30503\;enode://$PROXY_ENODE@$PROXY_EXTERNAL_IP:30303 \
-  --unlock=$CELO_VALIDATOR_SIGNER_ADDRESS --password /root/.celo/.password \
-  --celostats=${validator_name}@${ethstats_host} --baklava --datadir /root/.celo
+docker run -d --name celo-validator $DOCKER_LOGGING_PARAMS --restart unless-stopped --stop-timeout 300 -p 30303:30303 -p 30303:30303/udp -v $PWD:/root/.celo $CELO_IMAGE --verbosity 3 --syncmode full --mine --etherbase $CELO_VALIDATOR_SIGNER_ADDRESS --nodiscover --nousb  --proxy.proxied --proxy.proxyenodeurlpair=enode://$PROXY_ENODE@$PROXY_INTERNAL_IP:30503\;enode://$PROXY_ENODE@$PROXY_EXTERNAL_IP:30303 --unlock=$CELO_VALIDATOR_SIGNER_ADDRESS --password /root/.celo/.password --celostats=${validator_name}@${ethstats_host} --baklava --datadir /root/.celo
